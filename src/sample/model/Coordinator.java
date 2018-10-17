e43c7fd6abba19b032594af5548430b48e1c813c
PaigeWong <43482175+PaigeWong@users.noreply.github.com>
2018-10-17 15:31:16 -0400

    Update Coordinator.java

--- src/sample/model/Coordinator.java
+++ src/sample/model/Coordinator.java
@@ -1,10 +1,22 @@
 package sample.model;
 
+/**
+ * This class is to set coordinator location.
+ */
 public class Coordinator{
 
     private int x;
 
     private int y;
+   
+    /**
+	 * Constructor method with incoming parameters
+	 * 
+	 * @param x
+	 *            x coordinate location with int type
+	 * @param y
+	 *            y coordinate location with int type
+	 */
 
     public Coordinator(int x, int y) {
 
@@ -12,19 +24,37 @@ public class Coordinator{
 
         this.y = y;
     }
+    /**
+	 * To get the X coordinator
+	 * 
+	 * @return X with int type
+	 */
 
     public int getX() {
         return x;
     }
-
+    /**
+	 * To set the X coordinator
+	 * 
+	 * @param X with int type
+	 */
     public void setX(int x) {
         this.x = x;
     }
-
+    
+    /**
+	 * To get the Y coordinator
+	 * 
+	 * @return Y with int type
+	 */
     public int getY() {
         return y;
     }
-
+    /**
+	 * To set the Y coordinator
+	 * 
+	 * @param Y with int type
+	 */
     public void setY(int y) {
         this.y = y;
     }
