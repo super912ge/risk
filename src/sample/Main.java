c9c1e6a21412dbc14493d22dbb831549b6aeb10c
PaigeWong <43482175+PaigeWong@users.noreply.github.com>
2018-10-17 15:11:15 -0400

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,6 +8,10 @@ import javafx.stage.Stage;
 import sample.frontPage.FrontPageController;
 import sample.gamePage.GamePageController;
 
+/**
+ * The main entrance of the application
+ *
+ */
 public class Main extends Application {
 
     private Stage stage;
@@ -90,7 +94,10 @@ public class Main extends Application {
     public void setGamePageController(GamePageController gamePageController) {
         this.gamePageController = gamePageController;
     }
-
+	
+    /**
+	 * Launch the application.
+	 */
     public static void main(String[] args) {
         launch(args);
     }
